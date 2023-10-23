65c3d64cadc4d3c523a2103e5dc6cc5b7d4347be
ElectronicIV <louis.persin@epitech.eu>
2023-10-23 15:55:38 +0200

    Create core structs

---
+++ include/internal.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2023
+** Day07-Libmy, arguments
+** File description:
+** Task02 - concatenate two strings
+** non-printable characters are printed hexadecimally
+*/
+#ifndef NO_USE_INTERNAL
+    #define NO_USE_INTERNAL
+
+    #define MAX_SPEC_LEN 2
+
+typedef struct {
+    size_t written;
+    size_t width;
+    size_t prec;
+}print_info_t;
+
+typedef struct {
+    void (*handle)(print_info_t *info);
+    char name[MAX_SPEC_LEN];
+    type_t type;
+} spec_t
+
+
+#endif
