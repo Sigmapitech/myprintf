a790db914bc96f7213e8db37f46f13ae8fd621f7
ElectronicIV <louis.persin@epitech.eu>
2023-10-23 18:52:44 +0200

    Add util put_nbr

---
+++ utils.c
@@ -0,0 +1,21 @@
+int put_nbr(int fd, int nb)
+{
+    int out = nb / 10;
+    char c;
+
+    if (nb == 0)
+        return put_char('0');
+    if (nb < 0) {
+        c = '-';
+        write(fd, &c, 1);
+        out = out != 0 ? put_nbr(~out + 1) : 0;
+        out++;
+        c = ~(nb % 10) + 1 + '0';
+        write(fd, &c, 1);
+    } else {
+        out = out != 0 ? put_nbr(out) : 0;
+        c = nb % 10 + '0';
+        write(fd, &c, 1);
+    }
+    return out + 1;
+}
