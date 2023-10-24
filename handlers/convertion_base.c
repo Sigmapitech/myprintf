8f3eb1456e17e680c6ea224cd80d9b95a6b6c5c8
ElectronicIV <louis.persin@epitech.eu>
2023-10-24 13:02:16 +0200

    Fix compilation errors

--- handlers/convertion_base.c
+++ handlers/convertion_base.c
@@ -7,28 +7,28 @@
 
 #include "internal.h"
 
-int conv_char(print_info_t pinfo, conv_info cinfo)
+int conv_char(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
 
-int conv_int(print_info_t pinfo, conv_info cinfo)
+int conv_int(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
 
-int conv_str(print_info_t pinfo, conv_info cinfo)
+int conv_str(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
 
-int conv_ptr(print_info_t pinfo, conv_info cinfo)
+int conv_ptr(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
 
 // percent
-int conv_per(print_info_t pinfo, conv_info cinfo)
+int conv_per(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
--- handlers/convertion_float.c
+++ handlers/convertion_float.c
@@ -1,25 +1,25 @@
 #include "internal.h"
 
 // scientific notation of float
-int conv_nota_sci(print_info_t pinfo, conv_info cinfo)
+int conv_nota_sci(print_info_t pinfo, conv_inf_to cinfo)
 {
     return 0;
 }
 
 // decimal notation
-int conv_nota_dec(print_info_t pinfo, conv_info cinfo)
+int conv_nota_dec(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
 
 // variable between -f & -e
-int conv_nota_var(print_info_t pinfo, conv_info cinfo)
+int conv_nota_var(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
 
 // hexadeecima + scientific notation
-int conv_nota_hex(print_info_t pinfo, conv_info cinfo)
+int conv_nota_hex(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
