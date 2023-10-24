8a475fb32beb76a1561b9af9b006a4debcba3a3b
ElectronicIV <louis.persin@epitech.eu>
2023-10-24 13:02:16 +0200

    Fix compilation errors

--- handlers/convertion_extra.c
+++ handlers/convertion_extra.c
@@ -1,7 +1,7 @@
 #include "internal.h"
 
 // number of char printed
-int conv_num(print_info_t pinfo, conv_info cinfo)
+int conv_num(print_info_t pinfo, conv_info_t cinfo)
 {
     return 0;
 }
--- internal.h
+++ internal.h
@@ -13,7 +13,53 @@
     #include <stdarg.h>
     #include <stddef.h>
 
-typedef conv_func_t int (*handle)(int fd)
+typedef enum {
+    FLAG,
+    CONVERSION,
+    LENGTH_MODIFIER,
+} type_t;
+
+typedef struct {
+    int fd;
+    size_t written;
+    va_list ap;
+} print_info_t;
+
+typedef struct {
+    size_t width;
+    char flag;
+    char *len_mod;
+    int prec;
+} conv_info_t;
+
+typedef int (*conv_func_t)(print_info_t, conv_info_t);
+
+typedef struct {
+    conv_func_t *handler;
+    char name[MAX_SPEC_LEN];
+    type_t type;
+} spec_t;
+
+int put_nbr(int fd, int nb);
+int vdprintf(int fd, const char *restrict format, va_list ap);
+
+
+int conv_char(print_info_t pinfo, conv_info_t cinfo);
+int conv_int(print_info_t pinfo, conv_info_t cinfo);
+int conv_str(print_info_t pinfo, conv_info_t cinfo);
+int conv_ptr(print_info_t pinfo, conv_info_t cinfo);
+int conv_per(print_info_t pinfo, conv_info_t cinfo);
+
+int conv_oct(print_info_t pinfo, conv_info_t cinfo);
+int conv_hex(print_info_t pinfo, conv_info_t cinfo);
+int conv_uint(print_info_t pinfo, conv_info_t cinfo);
+
+int conv_nota_sci(print_info_t pinfo, conv_info_t cinfo);
+int conv_nota_dec(print_info_t pinfo, conv_info_t cinfo);
+int conv_nota_var(print_info_t pinfo, conv_info_t cinfo);
+int conv_nota_hex(print_info_t pinfo, conv_info_t cinfo);
+
+int conv_num(print_info_t pinfo, conv_info_t cinfo);
 
 static
 const conv_func_t CONVERSION_FUNCS[ CONV_IDX('z') ] = {
@@ -34,52 +80,6 @@ const conv_func_t CONVERSION_FUNCS[ CONV_IDX('z') ] = {
     [ CONV_IDX('A') ] = &conv_nota_hex,
     [ CONV_IDX('a') ] = &conv_nota_hex,
     [ CONV_IDX('n') ] = &conv_num
-}
-
-typedef struct {
-    int fd;
-    size_t written;
-    va_list ap;
-} print_info_t;
-
-typedef struct {
-    size_t width;
-    char flag;
-    char *len_mod;
-    int prec;
-} conv_info_t;
-
-typedef enum {
-    FLAG,
-    CONVERSION,
-    LENGTH_MODIFIER,
-} type_t;
-
-typedef struct {
-    void (*handle)(int fd, print_info_t *info);
-    char name[MAX_SPEC_LEN];
-    type_t type;
-} spec_t;
-
-int put_nbr(int fd, int nb);
-int vdprintf(int fd, const char *restrict format, va_list ap);
-
-
-int conv_char(print_info_t pinfo, conv_info cinfo);
-int conv_int(print_info_t pinfo, conv_info cinfo);
-int conv_str(print_info_t pinfo, conv_info cinfo);
-int conv_ptr(print_info_t pinfo, conv_info cinfo);
-int conv_per(print_info_t pinfo, conv_info cinfo);
-
-int conv_oct(print_info_t pinfo, conv_info cinfo);
-int conv_hex(print_info_t pinfo, conv_info cinfo);
-int conv_uint(print_info_t pinfo, conv_info cinfo);
-
-int conv_nota_sci(print_info_t pinfo, conv_info cinfo);
-int conv_nota_dec(print_info_t pinfo, conv_info cinfo);
-int conv_nota_var(print_info_t pinfo, conv_info cinfo);
-int conv_nota_hex(print_info_t pinfo, conv_info cinfo);
-
-int conv_num(print_info_t pinfo, conv_info cinfo);
+};
 
 #endif
