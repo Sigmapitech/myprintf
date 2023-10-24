2b0f7ab11390b37e8f6cc28531511e4f78925781
ElectronicIV <louis.persin@epitech.eu>
2023-10-24 12:46:18 +0200

    Add all handler prototypes

---
+++ handlers/convertion_extra.c
@@ -0,0 +1,7 @@
+#include "internal.h"
+
+// number of char printed
+int conv_num(print_info_t pinfo, conv_info cinfo)
+{
+    return 0;
+}
--- internal.h
+++ internal.h
@@ -8,23 +8,32 @@
     #define NO_USE_INTERNAL
 
     #define MAX_SPEC_LEN 2
+    #define CONV_IDX(c) (c - 'A')
 
     #include <stdarg.h>
     #include <stddef.h>
 
-union arg_type {
-    char c;
-    int i;
-    void *p;
-};
+typedef conv_func_t int (*handle)(int fd)
+
+static
+const conv_func_t CONVERSION_FUNCS[ CONV_IDX('z') ] = {
+    [ CONV_IDX('c') ] = &print_char,
+
+
+}
 
 typedef struct {
+    int fd;
     size_t written;
-    size_t width;
-    size_t prec;
-    va_list *ap;
-    union arg_type arg;
+    va_list ap;
 } print_info_t;
+
+typedef struct {
+    size_t width;
+    char flag;
+    char *len_mod;
+    int prec;
+} conv_info_t;
 
 typedef enum {
     FLAG,
@@ -41,8 +50,22 @@ typedef struct {
 int put_nbr(int fd, int nb);
 int vdprintf(int fd, const char *restrict format, va_list ap);
 
-int print_char(int fd, print_info_t pinfo);
-int print_int(int fd, print_info_t pinfo);
-int print_str(int fd, print_info_t pinfo);
+
+int conv_char(print_info_t pinfo, conv_info cinfo);
+int conv_int(print_info_t pinfo, conv_info cinfo);
+int conv_str(print_info_t pinfo, conv_info cinfo);
+int conv_ptr(print_info_t pinfo, conv_info cinfo);
+int conv_per(print_info_t pinfo, conv_info cinfo);
+
+int conv_oct(print_info_t pinfo, conv_info cinfo);
+int conv_hex(print_info_t pinfo, conv_info cinfo);
+int conv_uint(print_info_t pinfo, conv_info cinfo);
+
+int conv_nota_sci(print_info_t pinfo, conv_info cinfo);
+int conv_nota_dec(print_info_t pinfo, conv_info cinfo);
+int conv_nota_var(print_info_t pinfo, conv_info cinfo);
+int conv_nota_hex(print_info_t pinfo, conv_info cinfo);
+
+int conv_num(print_info_t pinfo, conv_info cinfo);
 
 #endif
