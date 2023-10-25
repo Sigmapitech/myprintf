f4a19435d771cea6d53ac0b703463f2605868ad2
Sigmanificient <edhyjox@gmail.com>
2023-10-25 19:54:43 +0200

    Remove all unused functions of lib/my

--- my.h
+++ my.h
@@ -26,43 +26,9 @@
 
     #include <stdarg.h>
 
-char **my_str_to_word_array(char const *str);
-char *my_revstr(char *str);
-char *my_strcapitalize(char *str);
-char *my_strcat(char *dest, char const *src);
-char *my_strcpy(char *dest, char const *src);
-char *my_strdup(char const *src);
-char *my_strlowcase(char *str);
-char *my_strncat(char *dest, char const *src, int nb);
-char *my_strncpy(char *dest, char const *src, int n);
-char *my_strstr(char *str, char const *to_find);
-char *my_strupcase(char *str);
-int my_compute_power_rec(int nb, int p);
-int my_compute_square_root(int nb);
-int my_find_prime_sup(int nb);
-int my_getnbr(char *str);
-int my_getnbr_base(char *str, char const *base);
-int my_is_prime(int nb);
-int my_isneg(int n);
-int my_put_nbr(int n);
-int my_putnbr_base(int nbr, char const *base);
-int my_putstr(char const *str);
-int my_showmem(char const *str, int size);
-int my_showstr(char const *str);
-int my_str_isalpha(char const *str);
-int my_str_islower(char const *str);
-int my_str_isnum(char const *str);
-int my_str_isprintable(char const *str);
-int my_str_isupper(char const *str);
-int my_strcmp(char const *s1, char const *s2);
 int my_stridx(const char *str, char c);
 int my_strlen(char const *str);
-int my_strncmp(char const *s1, char const *s2, int n);
-int my_strnlen(char const *str, int n);
-void my_putchar(char c);
-void my_puthex(int line, int pad);
-void my_sort_int_array(int *array, int size);
-void my_swap(int *a, int *b);
+
 // Delivery
 int my_printf(const char *format, ...);
 int my_vdprintf(int fd, const char *format, va_list ap);
