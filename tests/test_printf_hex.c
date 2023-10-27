63bbc7d1c4c3d11a8f5ae0b0ac7a3864c80a378f
Sigmanificient <edhyjox@gmail.com>
2023-10-27 02:57:45 +0200

    Handle oct & hex alt form

--- tests/test_printf_hex.c
+++ tests/test_printf_hex.c
@@ -21,3 +21,12 @@ Test(test_printf, up_hex_pad, .init = cr_redirect_stdout)
     my_printf("[%4X]\n", 3301);
     cr_assert_stdout_eq_str("[CE5 ]\n");
 }
+
+Test(test_printf, hex_alt, .init = cr_redirect_stdout)
+{
+    my_printf("[%#X]", 3301);
+    my_printf("[%#x]", 3301);
+    my_printf("[%#x]", 0);
+    my_printf("[%#X]\n", 0);
+    cr_assert_stdout_eq_str("[0XCE5][0xce5][0][0]\n");
+}
