bfba7fa02df947333a650554f034e4fcac5e5027
ElectronicIV <louis.persin@epitech.eu>
2023-11-02 11:08:28 +0100

    Minor adjustments for double_to_str

--- float/double_to_str.c
+++ float/double_to_str.c
@@ -34,28 +34,38 @@ double d_abs(double d)
 static
 int handle_non_numbers(char *out, dpart_t dpart)
 {
-    if (dpart.mentissa == 0)
-        return put_in_str(out, dpart.sign ? "-inf" : "inf");
-    return put_in_str(out, "nan");
+    if (dpart.mentissa != 0)
+        return put_in_str(out, "nan");
+    return put_in_str(out, dpart.sign ? "-inf" : "inf");
+}
+
+static
+dpart_t init_dpart(double d)
+{
+    dpart_t dpart = {
+        .sign = (BITS(d) >> 63) & 1,
+        .exponant = (BITS(d) >> 52) & 0x7ff,
+        .mentissa = BITS(d) & 0x000fffffffffffffL
+    };
+
+    return dpart;
 }
 
 int double_to_str(char *out, double d, unsigned int prec)
 {
     int itgr = (int)d_abs(d);
     int i = 0;
-    dpart_t dpart = { (BITS(d) >> 63) & 1, (BITS(d) >> 52) & 0x7ff,
-        BITS(d) & 0x000fffffffffffffL };
+    int nb_prec = my_pow(10, prec);
+    dpart_t dpart = init_dpart(d);
 
-    if (dpart.exponant == 0x7ff) {
+    if (dpart.exponant == 0x7ff)
         return handle_non_numbers(out, dpart);
-    }
     d = d_abs(d) - itgr;
-    i += (dpart.sign ? put_in_str(out, "-") : 0)
+    i += put_in_str(out, dpart.sign ? "-" : "")
         + my_putnbr(out + dpart.sign, itgr);
     if (prec) {
         itgr = i;
-        d = my_pow(10, prec) + d * my_pow(10, prec) +
-            ((d * my_pow(10, prec) - (int)(d * my_pow(10, prec))) >= 0.5);
+        d = (d + 1) * nb_prec + ((d * nb_prec - (int)(d * nb_prec)) >= 0.5);
         i += my_putnbr(out + i, (int)(d));
         out[itgr] = '.';
     }
