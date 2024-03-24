653d8e597c89fd7b5b4f4d2106f0da09267e456f
Sigmanificient <edhyjox@gmail.com>
2024-03-24 13:29:03 +0100

    Rewrote flake with flake-utils

--- flake.nix
+++ flake.nix
@@ -1,77 +1,76 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-        ]
-          (system: function nixpkgs.legacyPackages.${system});
-    in
-    {
-      formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        formatter = pkgs.nixpkgs-fmt;
 
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            criterion
-            gcc13
-            gcovr
-            glibc
-            gnumake
-            python310Packages.compiledb
-            valgrind
-          ];
-        };
-      });
-
-      packages = forAllSystems (pkgs: rec {
-        default = libmy;
-        libmy = pkgs.stdenv.mkDerivation rec {
-          name = "libmy.a";
-          src = ./.;
-
-          makeFlags = [ "CC=${pkgs.gcc13}/bin/gcc" ];
-          buildInputs = [ pkgs.gnumake ];
-
-          hardeningDisable = [ "format" "fortify" ];
-          enableParallelBuilding = true;
-
-          installPhase = ''
-            runHook preInstall
-
-            mkdir -p $out/lib
-            cp ${name} $out/lib/${name}
-
-            runHook postInstall
-          '';
+        devShells = {
+          default = pkgs.mkShell {
+            hardeningDisable = [ "all" ];
+            inputsFrom = pkgs.lib.attrsets.attrValues packages;
+            packages = with pkgs; [
+              criterion
+              gcc13
+              gcovr
+              glibc
+              gnumake
+              python310Packages.compiledb
+              valgrind
+            ];
+          };
         };
 
-        unit_tests = libmy.overrideAttrs(prev: rec {
-          name = "unit_tests";
-          buildInputs = prev.buildInputs ++ [ pkgs.criterion ];
-          buildPhase = ''
-            runHook preBuild
+        packages = rec {
+          default = libmy;
+          libmy = pkgs.stdenv.mkDerivation rec {
+            name = "libmy.a";
+            src = ./.;
 
-            make unit_tests NO_COV=1
+            makeFlags = [ "CC=${pkgs.gcc13}/bin/gcc" ];
+            buildInputs = [ pkgs.gnumake ];
 
-            runHook postBuild
-          '';
+            hardeningDisable = [ "format" "fortify" ];
+            enableParallelBuilding = true;
 
-          installPhase = ''
-            runHook preInstall
+            installPhase = ''
+              runHook preInstall
 
-            mkdir -p $out/bin
-            cp ${name} $out/bin/${name}
+              mkdir -p $out/lib
+              cp ${name} $out/lib/${name}
 
-            runHook postInstall
-          '';
+              runHook postInstall
+            '';
+          };
 
-        });
+          unit_tests = libmy.overrideAttrs (prev: rec {
+            name = "unit_tests";
+            buildInputs = prev.buildInputs ++ [ pkgs.criterion ];
+            buildPhase = ''
+              runHook preBuild
+
+              make unit_tests NO_COV=1
+
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+              cp ${name} $out/bin/${name}
+
+              runHook postInstall
+            '';
+
+          });
+        };
       });
-    };
 }
